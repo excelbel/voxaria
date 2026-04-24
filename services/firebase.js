5b007e8aa9a44e4bb835192e3efbb2d05222fb85
excelbel <excelbeloved@gmail.com>
2026-04-24 16:14:34 +0100

    update blog post

--- services/firebase.js
+++ services/firebase.js
@@ -1,8 +1,12 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../firebase-service-account.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
 
 module.exports = admin;
