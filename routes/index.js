4a1916e0f85cf7263738221a6a8b10bfe841af18
excelbel <excelbeloved@gmail.com>
2026-06-07 11:45:37 +0100

    update blog post

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
+
 const homeController = require("../src/controllers/homeController");
 
+/* Home Page */
 router.get("/", homeController.home);
+
+/* Single News Post */
+router.get("/news/:slug", homeController.singlePost);
 
 module.exports = router;
